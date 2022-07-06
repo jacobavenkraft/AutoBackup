0b734deaa1fdee92e194f588220079125f1e9358
jacobavenkraft <jacob@nequad.com>
2022-07-06 10:38:59 -0500

    Add user control for editing paths.

---
+++ AutoBackup/View/SinglePathEditControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AutoBackup.View
+{
+    /// <summary>
+    /// Interaction logic for SinglePathEditControl.xaml
+    /// </summary>
+    public partial class SinglePathEditControl : UserControl
+    {
+        public SinglePathEditControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
